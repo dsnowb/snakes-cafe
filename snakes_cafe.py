4a1509946b0d2a5a0a21c6962e55a6eee310af8f
Dave Snowberger <git@fartherout.org>
2018-03-19 18:25:25 -0700

    fixed flake8 issues

--- snakes_cafe.py
+++ snakes_cafe.py
@@ -1,9 +1,9 @@
 if __name__ == '__main__':
 
-    appetizers = ['Pop-Tarts','Breadsticks','Chimichangas']
-    entrees = ['Cream of Frog','Clam Chowder','Crab Rangoon']
-    desserts = ['Smarties','Mochi','Chocolate Circuits']
-    drinks = ['Sprite','Most Bitterest IPA Ever','Root Beer Float']
+    appetizers = ['Pop-Tarts', 'Breadsticks', 'Chimichangas']
+    entrees = ['Cream of Frog', 'Clam Chowder', 'Crab Rangoon']
+    desserts = ['Smarties', 'Mochi', 'Chocolate Circuits']
+    drinks = ['Sprite', 'Most Bitterest IPA Ever', 'Root Beer Float']
     categories = {
         'Appetizers': appetizers,
         'Entrees': entrees,
@@ -12,15 +12,13 @@ if __name__ == '__main__':
     }
     cart = {}
 
-    
     def print_welcome():
         print("Welcome to Snake's Cafe!\nPress q any time to exit\n\nOur menu:\n")
-        for cat,cat_list in categories.items():
+        for cat, cat_list in categories.items():
             print('\n{}'.format(cat.upper()))
             for item in cat_list:
                 print(item.title())
         print('\n{0}\n** What would you like to order? **\n{0}'.format('*'*35))
-
 
     def take_order():
         while 1:
@@ -34,13 +32,11 @@ if __name__ == '__main__':
                 cart[order] = cart[order] + 1 if order in cart else 1
                 print_cart()
 
-
     def print_cart():
         print('{0}CART{0}\n'.format('*'*12))
         for item, amount in cart.items():
             print('{}: {}'.format(item, amount))
         print('\n' + '*'*28)
-
 
     print_welcome()
     take_order()
